24c96b397939325b770b91be20d9ea9fd7d6fffc
James Thomas <jabt1998@gmail.com>
2021-04-12 15:55:15 +0100

    created clothing card folders

---
+++ Resources/ClothingCard/ClothingCard.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+import styles from "./ClothingCard.modules.scss"
+
+
+const ClothingCard = () => {
+  return <div>
+    <img src="" alt=""/>
+    <h2>Gear Name</h2>
+    <p>Price</p>
+    <p>Size</p>
+    <p>Verified</p>
+
+  </div>
+}
+
+export default ClothingCard 
