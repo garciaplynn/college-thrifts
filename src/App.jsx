6c24c750b49d3ca09ed62338ab059cc51e897012
charliedodds <charlesgdodds@hotmail.co.uk>
2021-04-13 15:21:02 +0100

    Create mock user for props

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 // import styles from './App.module.scss';
 
-// import EditProfile from './components/EditProfile';
+import EditProfile from './components/EditProfile';
 
-const App = () => (
-  <div className="App">
-    {/* <EditProfile user={{ user: 'Tim' }} /> */}
-
-  </div>
-);
+const App = () => {
+  const mockUser = {
+    name: 'Tim',
+    classOf: 2016,
+    university: {
+      name: 'Trinity College Dublin',
+      abbreviation: 'TCD',
+    },
+    socials: [
+      {
+        url: 'instagram.com/tim.starrr',
+        username: 'tim.starrr',
+      },
+    ],
+  };
+  return (
+    <div className="App">
+      <EditProfile user={mockUser} />
+    </div>
+  );
+};
 
 export default App;
--- src/components/EditProfile/EditProfile.jsx
+++ src/components/EditProfile/EditProfile.jsx
@@ -1,21 +1,21 @@
-// import React from 'react';
+import React from 'react';
 
-// const EditProfile = ({ user }) => {
-//   const { name } = user;
+const EditProfile = ({ user }) => {
+  const { name, university, classOf } = user;
+  const year = String(classOf).slice(2);
+  return (
+    <>
+      <header>
+        {/* IMG COMPONENT WILL GO HERE */}
+        <h2>Profile</h2>
+        <h3>{name}</h3>
+        <p>{`${university.name} ${university.abbreviation} '${year}`}</p>
+      </header>
+      <section>
+        <h3>About</h3>
+      </section>
+    </>
+  );
+};
 
-//   return (
-//     <>
-//       <header>
-//         <h2>Profile</h2>
-//         <h3>{name}</h3>
-//         {/* <p>{`${uniName} ${uniAbbreviation} ${classOf}`}</p> */}
-//       </header>
-
-//       <main>
-
-//       </main>
-//     </>
-//   );
-// };
-
-// export default EditProfile;
+export default EditProfile;
