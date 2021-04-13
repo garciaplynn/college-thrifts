95a3078ba58d03b4c69d5c1fa536b4397aaeee3d
Tim Starr <timstarr14@gmail.com>
2021-04-13 14:54:27 +0100

    Start building edit profile component

--- src/components/EditProfile/EditProfile.jsx
+++ src/components/EditProfile/EditProfile.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+// import React from 'react';
 
-const EditProfile = () => (
-  <div>
-    <h2>Edit Profile</h2>
-  </div>
-);
+// const EditProfile = ({ user }) => {
+//   const { name } = user;
 
-export default EditProfile;
+//   return (
+//     <>
+//       <header>
+//         <h2>Profile</h2>
+//         <h3>{name}</h3>
+//         {/* <p>{`${uniName} ${uniAbbreviation} ${classOf}`}</p> */}
+//       </header>
+
+//       <main>
+
+//       </main>
+//     </>
+//   );
+// };
+
+// export default EditProfile;
