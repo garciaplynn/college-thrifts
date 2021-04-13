cc6f2b697add4811b8c364a966955408b188d4a9
Tim Starr <timstarr14@gmail.com>
2021-04-13 13:46:42 +0100

    Fix blank line

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import styles from './App.module.scss';
-
+// import styles from './App.module.scss';
 
 const App = () => (
   <div className="App">
