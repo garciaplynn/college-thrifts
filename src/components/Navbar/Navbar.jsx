89af614d751f3c8777d27af87e76325cf241abe9
charlie lansdale <charlielansdale@charlies-mbp.home>
2021-04-13 15:21:37 +0100

    navbar built

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUserCircle, faComments } from '@fortawesome/free-solid-svg-icons';
+// import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+// import { faComments } from '@fortawesome/free-solid-svg-icons';
 import styles from './Navbar.module.scss';
+// import ReactDOM from 'react-dom';
 
 const Navbar = () => (
   <nav className={styles.navBar}>
-    <button type="button" id={styles.homeButton}><a href="https://google.com">Home</a></button>
-    <button type="button" id={styles.profileButton}><a href="https://google.com">Profile</a></button>
-    <button type="button" id={styles.messageButton}><a href="https://google.com">Message</a></button>
+    <a id={styles.homeButton} className={styles.navbtn} href="https://google.com"><FontAwesomeIcon icon={faHome} label="homeButton" /></a>
+    <a type="button" id={styles.messageButton} className={styles.navbtn} href="https://google.com"><FontAwesomeIcon icon={faComments} label="messageButton" /></a>
+    <a type="button" id={styles.profileButton} className={styles.navbtn} href="https://google.com"><FontAwesomeIcon icon={faUserCircle} label="profileButton" /></a>
   </nav>
 );
 
