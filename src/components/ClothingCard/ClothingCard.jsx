38a7a845f3cf223db27af4c34920df24140397d1
charliedodds <charlesgdodds@hotmail.co.uk>
2021-04-14 15:45:57 +0100

    Fix merge bugs

--- src/components/ClothingCard/ClothingCard.jsx
+++ src/components/ClothingCard/ClothingCard.jsx
@@ -5,26 +5,31 @@ import styles from './ClothingCard.module.scss';
 
 const ClothingCard = (props) => {
   const { clothingItem } = props;
-  const { strImage, strPrice, strSize, strType } = clothingItem;
+  const {
+    strImage,
+    strPrice,
+    strSize,
+    strType,
+  } = clothingItem;
 
   return (
     <div className={styles.ClothingCards}>
-      <div className={styles.ImageCard}>
-        <img src={strImage} alt="Clothing" />
+      <div className="ClothingCards">
+        <div className={styles.ImageCard}>
+          <img src={strImage} alt="Clothing" />
+        </div>
+        <section className={styles.clothingSection}>
+          <div className={styles.clothingInfo}>
+            <h3>{strType}</h3>
+            <p>
+              {`Size: ${strSize}`}
+            </p>
+          </div>
+          <div>
+            <h2>{strPrice}</h2>
+          </div>
+        </section>
       </div>
-      <section className={styles.clothingSection}>
-        <div className={styles.clothingInfo}>
-          <h3>{strType}</h3>
-          <p>
-            Size:
-            <> </>
-            {strSize}
-          </p>
-        </div>
-        <div>
-          <h2>{strPrice}</h2>
-        </div>
-      </section>
     </div>
   );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faUserCircle, faComments } from '@fortawesome/free-solid-svg-icons';
+import {
+  faHome,
+  faUserCircle,
+  faComments,
+} from '@fortawesome/free-solid-svg-icons';
 import styles from './Navbar.module.scss';
 
 const Navbar = () => (
   <nav className={styles.Navbar}>
-    <a id={styles.homeButton} className={styles.navBtn} href="https://google.com"><FontAwesomeIcon icon={faHome} label="homeButton" /></a>
-    <a id={styles.messageButton} className={styles.navBtn} href="https://google.com"><FontAwesomeIcon icon={faComments} label="messageButton" /></a>
-    <a id={styles.profileButton} className={styles.navBtn} href="https://google.com"><FontAwesomeIcon icon={faUserCircle} label="profileButton" /></a>
+    <a
+      id={styles.homeButton}
+      className={styles.navBtn}
+      href="https://google.com"
+    >
+      <FontAwesomeIcon icon={faHome} label="homeButton" />
+    </a>
+    <a
+      id={styles.messageButton}
+      className={styles.navBtn}
+      href="https://google.com"
+    >
+      <FontAwesomeIcon icon={faComments} label="messageButton" />
+    </a>
+    <a
+      id={styles.profileButton}
+      className={styles.navBtn}
+      href="https://google.com"
+    >
+      <FontAwesomeIcon icon={faUserCircle} label="profileButton" />
+    </a>
   </nav>
 );
 
