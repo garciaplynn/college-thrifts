09581041186470b39cae2d9bce20b706bd8adadf
James Thomas <jabt1998@gmail.com>
2021-04-14 14:38:47 +0100

    finished adding styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+
 import ClothingCard from './components/ClothingCard';
 import fakeClothingData from './resources/data/fake-data';
 import styles from './components/ClothingCard/ClothingCard.module.scss';
@@ -8,7 +8,7 @@ import Navbar from './components/Navbar';
 const App = () => (
   <>
     <div className={styles.ClothingCards}>
-     <ClothingCard clothingItem={fakeClothingData[0]} />
+      <ClothingCard clothingItem={fakeClothingData[0]} />
     </div>
     <Navbar />
   </>
