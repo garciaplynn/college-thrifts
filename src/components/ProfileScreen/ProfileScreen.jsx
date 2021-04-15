ee632c172d3582c2cc4390398ea23c8c104be952
James Thomas <jabt1998@gmail.com>
2021-04-15 16:12:34 +0100

    added vh

--- src/components/ProfileScreen/ProfileScreen.jsx
+++ src/components/ProfileScreen/ProfileScreen.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import styles from './ProfileScreen.module.scss';
 
 const ProfileScreen = (props) => {
-  const { strImage } = props;
+  const { clothingItem } = props;
+  const {
+    strImage,
+  } = clothingItem;
 
   return (
-    <section>
-      <img src={strImage} alt="jumper" />
-    </section>
+
+    <>
+      <div className={styles.ClothingImagePage}>
+        <div className={styles.ClothingImage}>
+          <img src={strImage} alt="jumper" />
+        </div>
+      </div>
+      <h2>Hello</h2>
+    </>
   );
 };
 
