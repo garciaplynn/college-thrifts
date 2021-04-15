3f356bc70f2380738dde6a9f5b53d27e6ef713f6
Tim Starr <timstarr14@gmail.com>
2021-04-15 17:05:30 +0100

    Add styles for link bar

--- src/components/ProfileScreen/ProfileScreen.jsx
+++ src/components/ProfileScreen/ProfileScreen.jsx
@@ -1,21 +1,37 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCog, faTag, faHeart } from '@fortawesome/free-solid-svg-icons';
 import styles from './ProfileScreen.module.scss';
 
 const ProfileScreen = (props) => {
   const { clothingItem } = props;
-  const {
-    strImage,
-  } = clothingItem;
+  const { strImage } = clothingItem;
 
   return (
-
     <>
-      <div className={styles.ClothingImagePage}>
+      <section className={styles.profilePage}>
+        <article>
+          <a href="google.co.uk">
+            <FontAwesomeIcon icon={faHeart} />
+            Likes
+          </a>
+          <section className={styles.switch}>
+            <input type="checkbox" />
+            <span className={styles.slider} />
+          </section>
+          <a href="google.co.uk">
+            <FontAwesomeIcon icon={faTag} />
+            Selling
+          </a>
+          <a href="google.co.uk">
+            <FontAwesomeIcon icon={faCog} />
+            Settings
+          </a>
+        </article>
         <div className={styles.ClothingImage}>
           <img src={strImage} alt="jumper" />
         </div>
-      </div>
-      <h2>Hello</h2>
+      </section>
     </>
   );
 };
