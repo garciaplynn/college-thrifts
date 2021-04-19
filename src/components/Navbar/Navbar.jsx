5e20b16f9ddfbfda1dcb5ef41d5227d37844581f
charlie lansdale <charlielansdale@charlies-mbp.home>
2021-04-19 16:05:27 +0100

    icons added to fa library

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,5 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faTag,
-  faCamera,
-  faUserCircle,
-  faComments,
-  faGlobe,
-} from '@fortawesome/free-solid-svg-icons';
 import styles from './Navbar.module.scss';
 
 const Navbar = () => (
@@ -16,35 +9,35 @@ const Navbar = () => (
       className={styles.navBtn}
       href="https://google.com"
     >
-      <FontAwesomeIcon icon={faTag} label="shoppingButton" />
+      <FontAwesomeIcon icon="tag" label="shoppingButton" />
     </a>
     <a
       id={styles.messageButton}
       className={styles.navBtn}
       href="https://google.com"
     >
-      <FontAwesomeIcon icon={faComments} label="messageButton" />
+      <FontAwesomeIcon icon="comments" label="messageButton" />
     </a>
     <a
       id={styles.cameraButton}
       className={styles.navBtn}
       href="https://google.com"
     >
-      <FontAwesomeIcon icon={faCamera} label="cameraButton" />
+      <FontAwesomeIcon icon="camera" label="cameraButton" />
     </a>
     <a
       id={styles.carbonFootprintButton}
       className={styles.navBtn}
       href="https://google.com"
     >
-      <FontAwesomeIcon icon={faGlobe} label="carbonFootprintButton" />
+      <FontAwesomeIcon icon="globe" label="carbonFootprintButton" />
     </a>
     <a
       id={styles.profileButton}
       className={styles.navBtn}
       href="https://google.com"
     >
-      <FontAwesomeIcon icon={faUserCircle} label="profileButton" />
+      <FontAwesomeIcon icon="user-circle" label="profileButton" />
     </a>
   </nav>
 );
