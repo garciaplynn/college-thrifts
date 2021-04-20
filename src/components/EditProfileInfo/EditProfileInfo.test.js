9661448878553e0ac69072928715ab6b0088fa89
charliedodds <charlesgdodds@hotmail.co.uk>
2021-04-20 14:20:40 +0100

    Pass tests

--- src/components/EditProfileInfo/EditProfileInfo.test.js
+++ src/components/EditProfileInfo/EditProfileInfo.test.js
@@ -2,8 +2,10 @@ import React from "react";
 import { render } from "@testing-library/react";
 import EditProfileInfo from "./EditProfileInfo";
 
+import users from '../../resources/data/users';
+
 describe("EditProfileInfo tests", () => {
   it("should render", () => {
-    expect(render(<EditProfileInfo />)).toBeTruthy();
+    expect(render(<EditProfileInfo user={users[0]} />)).toBeTruthy();
   });
 });
