4a055c8a50385355cedce27f24561455adf5009d
Kinte Matulyte <=>
2021-04-20 16:48:46 +0100

    merge conflicts again

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,6 @@ import React from 'react';
 import ClothingRail from './containers/ClothingRail';
 import users from './resources/data/users';
 import './data/fa-library';
-<<<<<<< HEAD
-import EditProfile from './components/EditProfile';
-import Navbar from './components/Navbar';
-// import uniData from './resources/data/university-data';
-
-const App = () => (
-  <>
-    {/* <ClothingRailCard uniData={uniData[1]} />
-    <ClothingRailCard uniData={uniData[2]} />
-    <ClothingRailCard uniData={uniData[3]} /> */}
-    {/* <ClothingRailCard uniData={uniData[4]} /> */}
-    <ClothingRail />
-    <EditProfile user={users[0]} />
-    {/* <ProfileTop user={users[0]} uni={uniData[2]} /> */}
-=======
 import PrivateProfilePage from './components/PrivateProfilePage';
 import ClothingCard from './components/ClothingCard';
 import fakeClothingData from './resources/data/fake-data';
@@ -35,7 +20,6 @@ const App = () => (
     <PrivateProfilePage user={users[0]} />
     <ProfileTop user={users[0]} uni={uniData[2]} />
     <ProfileBottom clothingItem={fakeClothingData[0]} />
->>>>>>> 6ba39656230e401d6623a874a26e708f1767f9f6
     <Navbar />
   </>
 );
