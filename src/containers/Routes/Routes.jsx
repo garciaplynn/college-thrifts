3bd35b5e1c55313b7f04b1f740386a2d0f6ea05c
Michael Ferreira <ferreira.mjm@gmail.com>
2021-04-20 16:30:34 +0100

    Switch user

--- src/containers/Routes/Routes.jsx
+++ src/containers/Routes/Routes.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Router } from '@reach/router';
-// import ClothingRail from '../../components/ClothingRail';
+import ClothingRail from '../../components/ClothingRail';
 import ProfileTop from '../../components/ProfileTop';
 import CarbonFootprint from '../CarbonFootprint';
 import MessageInbox from '../MessageInbox';
 import UploadPicture from '../UploadPicture';
-import Data from '../../data'
+// import uniData from '../../resources/data/university-data';
 
 const Routes = () => (
   <Router>
-    {/* <ClothingRail path="clothing-rail" /> */}
+    <ClothingRail path="clothing-rail" />
     <MessageInbox path="message-inbox" />
     <UploadPicture path="upload-picture" />
     <CarbonFootprint path="carbon-footprint" />
