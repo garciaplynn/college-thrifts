9e4f6c9f339dcdaf077901e007a22d6f1d31fcbd
charlie lansdale <charlielansdale@charlies-mbp.home>
2021-04-20 16:58:53 +0100

    Navigation links complete

--- src/containers/Routes/Routes.jsx
+++ src/containers/Routes/Routes.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Router } from '@reach/router';
 import ClothingRail from '../../components/ClothingRail';
+import users from '../../resources/data/users';
 import ProfileTop from '../../components/ProfileTop';
 import CarbonFootprint from '../CarbonFootprint';
 import MessageInbox from '../MessageInbox';
 import UploadPicture from '../UploadPicture';
-// import uniData from '../../resources/data/university-data';
+import uniData from '../../resources/data/university-data';
 
 const Routes = () => (
   <Router>
-    <ClothingRail path="clothing-rail" />
+    <ClothingRail path="clothing-rail" uniData={uniData[1]} />
     <MessageInbox path="message-inbox" />
     <UploadPicture path="upload-picture" />
     <CarbonFootprint path="carbon-footprint" />
-    <ProfileTop path="profile" />
+    <ProfileTop path="profile" user={users[0]} uni={uniData[2]} />
   </Router>
 );
 
