107137721c35d3f62bd13fe08c4a981c8df4e866
charlie lansdale <charlielansdale@charlies-mbp.home>
2021-04-21 16:54:37 +0100

    inbox picture removed

--- src/containers/MessageInbox/MessageInbox.jsx
+++ src/containers/MessageInbox/MessageInbox.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import styles from './MessageInbox.module.scss';
-import image from './message.svg';
 
 const MessageInbox = () => (
   <>
     <p>MessageInbox works</p>
-    <img className={styles.image} src={image} alt="" />
   </>
 );
 
