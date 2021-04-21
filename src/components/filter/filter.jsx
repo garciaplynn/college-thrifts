9c093787a8aa2f921c6f3e758cb157826be2976b
TochiAbara <tochiabara@icloud.com>
2021-04-21 15:18:32 +0100

    started making the form for he filter button

--- src/components/filter/filter.jsx
+++ src/components/filter/filter.jsx
@@ -5,6 +5,30 @@ import styles from './Filter.module.scss';
 const Filter = () => (
   <div className={styles.Filter}>
     <FontAwesomeIcon icon="sort-down" />
+    <form>
+      <p className={styles.heading}>Gender:</p>
+      <label htmlFor="mens">
+        <input className={styles.input} type="checkbox" name="gender" value="mens" id="mens" />
+        Mens
+      </label>
+      <label htmlFor="womens">
+        <input className={styles.input} type="checkbox" name="gender" value="womens" id="womens" />
+        Womens
+      </label>
+      <p className={styles.heading}>Condition</p>
+      <label htmlFor="as-new">
+        <input type="checkbox" name="condition" value="as-new" id="as-new" />
+        As new
+      </label>
+      <label htmlFor="used">
+        <input className={styles.input} type="checkbox" name="condition" value="used" id="used" />
+        Used
+      </label>
+      <label htmlFor="vintage">
+        <input type="checkbox" name="condition" value="vintage" id="vintage" />
+        Vintage
+      </label>
+    </form>
   </div>
 );
 export default Filter;
