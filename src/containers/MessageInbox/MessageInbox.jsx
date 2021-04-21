2fa1e95cc16192483c766665bfaf67845d40e6b8
Michael Ferreira <ferreira.mjm@gmail.com>
2021-04-21 15:35:12 +0100

    Adding placeholder images

--- src/containers/MessageInbox/MessageInbox.jsx
+++ src/containers/MessageInbox/MessageInbox.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import './MessageInbox.module.scss';
 
 const MessageInbox = () => (
   <>
     <p>MessageInbox works</p>
+    <img src="../../resources/MessageList.jpg" alt="" />
   </>
 );
 
