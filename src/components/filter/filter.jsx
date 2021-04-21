59cc6ea61f882af3c6922d986bcef9fdf23ec4d1
Tim Starr <timstarr14@gmail.com>
2021-04-21 16:06:09 +0100

    Add styles and new labels

--- src/components/filter/filter.jsx
+++ src/components/filter/filter.jsx
@@ -3,32 +3,98 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './Filter.module.scss';
 
 const Filter = () => (
-  <div className={styles.Filter}>
-    <FontAwesomeIcon icon="sort-down" />
-    <form>
-      <p className={styles.heading}>Gender:</p>
-      <label htmlFor="mens">
-        <input className={styles.input} type="checkbox" name="gender" value="mens" id="mens" />
-        Mens
-      </label>
-      <label htmlFor="womens">
-        <input className={styles.input} type="checkbox" name="gender" value="womens" id="womens" />
-        Womens
-      </label>
-      <p className={styles.heading}>Condition</p>
-      <label htmlFor="as-new">
-        <input type="checkbox" name="condition" value="as-new" id="as-new" />
-        As new
-      </label>
-      <label htmlFor="used">
-        <input className={styles.input} type="checkbox" name="condition" value="used" id="used" />
-        Used
-      </label>
-      <label htmlFor="vintage">
-        <input type="checkbox" name="condition" value="vintage" id="vintage" />
-        Vintage
-      </label>
-    </form>
-  </div>
+  <section className={styles.container}>
+    <div className={styles.Filter}>
+      <FontAwesomeIcon icon="sort-down" className={styles.icon} />
+      <form>
+        <p className={styles.heading}>Gender:</p>
+        <label htmlFor="mens">
+          <input
+            className={styles.input}
+            type="checkbox"
+            name="gender"
+            value="mens"
+            id="mens"
+          />
+          Mens
+        </label>
+        <label htmlFor="womens">
+          <input
+            className={styles.input}
+            type="checkbox"
+            name="gender"
+            value="womens"
+            id="womens"
+          />
+          Womens
+        </label>
+        <p className={styles.heading}>Condition:</p>
+        <label htmlFor="as-new">
+          <input type="checkbox" name="condition" value="as-new" id="as-new" />
+          As new
+        </label>
+        <label htmlFor="used">
+          <input
+            className={styles.input}
+            type="checkbox"
+            name="condition"
+            value="used"
+            id="used"
+          />
+          Used
+        </label>
+        <label htmlFor="vintage">
+          <input
+            type="checkbox"
+            name="condition"
+            value="vintage"
+            id="vintage"
+          />
+          Vintage
+        </label>
+        <p className={styles.heading}>Style:</p>
+        <label htmlFor="style">
+          <input
+            className={styles.input}
+            type="checkbox"
+            name="style"
+            value="crewneck"
+            id="crewneck"
+          />
+          Crewneck
+        </label>
+        <label htmlFor="style">
+          <input
+            className={styles.input}
+            type="checkbox"
+            name="style"
+            value="hoodie"
+            id="hoodie"
+          />
+          Hoodie
+        </label>
+        <label htmlFor="style">
+          <input
+            className={styles.input}
+            type="checkbox"
+            name="style"
+            value="jersey"
+            id="jersey"
+          />
+          Jersey
+        </label>
+        <label htmlFor="style">
+          <input
+            className={styles.input}
+            type="checkbox"
+            name="style"
+            value="jacket"
+            id="jacket"
+          />
+          Jacket
+        </label>
+      </form>
+    </div>
+  </section>
 );
 export default Filter;
