607c9f5d211ad6715ae759a569874b81ea6dd4df
TochiAbara <tochiabara@icloud.com>
2021-04-22 16:11:53 +0100

    added some tests to the filter button

--- src/components/FilterButton/FilterButton.test.js
+++ src/components/FilterButton/FilterButton.test.js
@@ -2,19 +2,21 @@ import React, { Component } from 'react';
 import { render } from "@testing-library/react";
 import Filter from './FilterButton';
 import { shallow } from 'enzyme';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 describe("filter tests", () => {
   let component;
   let testForm = "Test form";
 
+  beforeEach(() => {
+    component = shallow( <Filter />)
+  })
+
   it("should render", () => {
     expect(render(<Filter />)).toBeTruthy();
   });
+
   it("should display a form once the icon is clicked", () => {
     component.find('FontAwesomeIcon').simulate('click');
     expect(component.find('form')).toBeTruthy();
   })
-
-
 });
