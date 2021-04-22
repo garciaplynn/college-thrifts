326405fce929b0135333ea06df2a76da7433d565
joe-fol <joefolkard@protonmail.com>
2021-04-22 14:46:11 +0100

    Test

--- src/components/ClothingRail/ClothingRail.test.js
+++ src/components/ClothingRail/ClothingRail.test.js
@@ -1,16 +1,28 @@
 import ClothingRail from './ClothingRail';
 import Button from '../Button';
 import { shallow } from 'enzyme';
+import users from '../../resources/data/users';
+import uniData from '../../resources/data/university-data';
 
 describe('ClothingRail tests', () => {
 
   let component;
   
   beforeEach(() => {
-    component = shallow(<ClothingRail />);
+    component = shallow(<ClothingRail user={users[0]} uniData={uniData[0]}  />);
   });
 
   it('should render buttons', () => {
     expect(component.find(Button)).toBeTruthy();
   });
+
+  it('should add correct item to the like page', () => {
+    expect(component.props().user.likes.length).toBe(0);
+
+    const button = component.find(Button);
+    button.find('button').at(0).simulate('click');
+
+    expect(component.props().user.likes.length).toBe(1);
+
+  })
 });
