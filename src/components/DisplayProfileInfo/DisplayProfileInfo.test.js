d2a88120e1c5caa6c88c64f3e39cc74916ba9713
charliedodds <charlesgdodds@hotmail.co.uk>
2021-04-27 14:32:41 +0100

    Add input tests to EditProfileInfo

--- src/components/DisplayProfileInfo/DisplayProfileInfo.test.js
+++ src/components/DisplayProfileInfo/DisplayProfileInfo.test.js
@@ -1,10 +1,10 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import DisplayProfileInfo from "./DisplayProfileInfo";
+import React from 'react';
+import { render } from '@testing-library/react';
+import DisplayProfileInfo from './DisplayProfileInfo';
 import users from '../../resources/data/users';
 import { mount } from 'enzyme';
 
-describe("DisplayProfileInfo tests", () => {
+describe('DisplayProfileInfo tests', () => {
   let component;
 
   beforeEach(() => {
@@ -12,22 +12,22 @@ describe("DisplayProfileInfo tests", () => {
   });
 
   it('should render', () => {
+    expect(render(<DisplayProfileInfo user={users[0]} />)).toBeTruthy();
+  });
+
+  it('should render user name', () => {
     expect(component.find('p').at(0).text()).toBe('Tim Starr');
   });
 
-  it('should render', () => {
+  it('should render user following', () => {
     expect(component.find('p').at(2).text()).toBe('3 friends');
   });
 
-  it('should render', () => {
+  it('should render user followers', () => {
     expect(component.find('p').at(3).text()).toBe('100000000 friends');
   });
 
-  it('should render', () => {
+  it('should render social URLS', () => {
     expect(component.find('li').at(0).text()).toBe('instagram.com/tim.starrr');
-  });
-
-  it("should render", () => {
-    expect(render(<DisplayProfileInfo user={users[0]} />)).toBeTruthy();
   });
 });
--- src/components/EditProfileInfo/EditProfileInfo.test.js
+++ src/components/EditProfileInfo/EditProfileInfo.test.js
@@ -1,11 +1,31 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import EditProfileInfo from "./EditProfileInfo";
+import React from 'react';
+import EditProfileInfo from './EditProfileInfo';
+import { shallow } from 'enzyme';
 
 import users from '../../resources/data/users';
 
-describe("EditProfileInfo tests", () => {
-  it("should render", () => {
-    expect(render(<EditProfileInfo user={users[0]} />)).toBeTruthy();
+describe('EditProfileInfo tests', () => {
+  let component;
+
+  beforeEach(() => {
+    component = shallow(<EditProfileInfo user={users[0]} />);
+  });
+
+  it('should render', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should render input elements', () => {
+    expect(component.find('input').length).toBe(2);
+  });
+
+  it("should set the value of the first input to be the user's name", () => {
+    expect(component.find('input').at(0).prop('value')).toBe('Tim Starr');
+  });
+
+  it("should set the value of the second input to be the user's email", () => {
+    expect(component.find('input').at(1).prop('value')).toBe(
+      'timstarr14@gmail.com'
+    );
   });
 });
