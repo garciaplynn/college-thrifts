ed79ce89e8fc5c8e0c56f321a0f6520cf5255397
charlie lansdale <charlielansdale@charlies-mbp.home>
2021-04-27 14:43:44 +0100

    navbar icons tested

--- src/containers/PrivateProfile/PrivateProfile.test.js
+++ src/containers/PrivateProfile/PrivateProfile.test.js
@@ -6,14 +6,9 @@ import users from '../../resources/data/users';
 import { shallow } from 'enzyme';
 
 describe("PrivateProfile tests", () => {
-<<<<<<< HEAD
-  it("should render", () => {
-    expect(render(<PrivateProfile user={user} toggleIsEditing={toggleIsEditing} />)).toBeTruthy();
-=======
   it('should render', () => {
     const mockFunction = jest.fn();
     expect(render(<PrivateProfile user={users[0]} toggleIsEditing={mockFunction} />)).toBeTruthy();
->>>>>>> 8e76da41f878a753ee7e9e2002a4f199cc9409c9
   });
 
 });
