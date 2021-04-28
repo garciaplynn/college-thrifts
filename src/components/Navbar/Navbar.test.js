d323069a16cbb29209ef30df362cd25c84302aed
Michael Ferreira <ferreira.mjm@gmail.com>
2021-04-28 14:31:05 +0100

    Fixing tests

--- src/components/Navbar/Navbar.test.js
+++ src/components/Navbar/Navbar.test.js
@@ -1,22 +1,6 @@
-<<<<<<< HEAD
-import React from "react";
-import Navbar from "./Navbar";
-import { shallow } from 'enzyme';
-
-describe("Navbar tests", () => {
-  let component;
-
-  beforeEach(() => {
-    component = shallow(<Navbar icon="" label=""/>);
-  })
-
-  it('should render', () => {
-    expect(component).toBeTruthy();
-  });
-
-=======
 import React from 'react';
 import Navbar from './Navbar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { shallow } from 'enzyme';
 
 describe('Navbar tests', () => {
@@ -27,5 +11,23 @@ describe('Navbar tests', () => {
   it('should render', () => {
     expect(component).toBeTruthy();
   })
->>>>>>> 4a70ae1d80f8d6424dd9045642db043c8bd628bc
+  it('has 5 icons in the navbar', () => {
+    expect(component.find(FontAwesomeIcon).length).toBe(5);
+  })
+  it('must show tag icon for first icon', () => {
+    expect(component.find(FontAwesomeIcon).at(0).prop('icon')).toBe('tag')
+  })
+  it('must show message icon for second icon', () => {
+    expect(component.find(FontAwesomeIcon).at(1).prop('icon')).toBe('comments')
+  })
+  it('must show camera icon for third icon', () => {
+    expect(component.find(FontAwesomeIcon).at(2).prop('icon')).toBe('camera')
+  })
+  it('must show globe icon for fourth icon', () => {
+    expect(component.find(FontAwesomeIcon).at(3).prop('icon')).toBe('globe-americas')
+  })
+  it('must show profile icon for first icon', () => {
+    expect(component.find(FontAwesomeIcon).at(4).prop('icon')).toBe('user-circle')
+  })
+
 });
