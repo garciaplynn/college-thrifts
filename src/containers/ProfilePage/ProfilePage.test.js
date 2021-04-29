b093de1132088c7058f2d5da4ac42ed1cb19c5ee
James Thomas <jabt1998@gmail.com>
2021-04-29 17:02:26 +0100

    tests

--- src/containers/ProfilePage/ProfilePage.test.js
+++ src/containers/ProfilePage/ProfilePage.test.js
@@ -6,8 +6,17 @@ import uniData from '../../resources/data/university-data';
 import data from '../../resources/data/fake-data';
 
 
+
+
 describe('ProfilePage tests', () => {
   it('should render', () => {
     expect(render(<ProfilePage user={users[0]} uni={uniData[2]} clothingItem={data[0]} />)).toBeTruthy();
   });
 });
+
+describe('render the popup on the page', () => {
+  beforeEach(() => {
+    componenet = mount(<Filter />)
+  })
+
+})
