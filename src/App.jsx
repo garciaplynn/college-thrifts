6e9ac5346cc69dcc0ee9199355c0dc92cb7c0db5
charliedodds <charlesgdodds@hotmail.co.uk>
2021-05-04 15:05:46 +0100

    Implement firebase and seed database

--- src/App.jsx
+++ src/App.jsx
@@ -8,34 +8,15 @@ import Routes from './containers/Routes';
 const App = () => {
   const [user, setUser] = useState(null);
 
-  const addUniversityData = (uniObject) => {
-    firestore
-      .collection('university')
-      .doc()
-      .set(uniObject)
-      .then(() => {
-        console.log('Document successfully written');
-      })
-      .catch((error) => {
-        console.log('Error when writing document: ', error);
-      });
-  };
-
-data.forEach(uni => console.log(uni));
-
   useEffect(() => {
-    console.log('in use effect');
     firestore
       .collection('users')
       .doc('test-user')
       .get()
       .then((newUser) => {
-        console.log('in firestore function');
         setUser(newUser.data());
-        console.log(newUser.data());
       })
       .catch((err) => {
-        console.log('in error');
         console.log(err);
       });
   }, []);
