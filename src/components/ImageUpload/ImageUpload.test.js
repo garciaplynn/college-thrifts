18430a385d940eea5a513af28098d9784fc67225
TochiAbara <tochiabara@icloud.com>
2021-05-05 15:27:47 +0100

    styling the form

--- src/components/ImageUpload/ImageUpload.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import ImageUpload from "./ImageUpload";
-
-describe("ImageUpload tests", () => {
-  it("should render", () => {
-    expect(render(<ImageUpload />)).toBeTruthy();
-  });
-});
