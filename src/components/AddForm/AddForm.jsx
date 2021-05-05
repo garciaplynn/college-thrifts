c20977ca222e583275e9ff949488e9e4886a81f1
TochiAbara <tochiabara@icloud.com>
2021-05-05 17:10:01 +0100

    commit all changes

--- src/components/AddForm/AddForm.jsx
+++ src/components/AddForm/AddForm.jsx
@@ -5,8 +5,10 @@ const AddForm = () => (
   <div className="formContainer">
     <h3 className="formHeading">Upload an item</h3>
     <form>
-      <p className="categories">Image:</p>
-      <input type="file" />
+      <div className="imageCheckboxes">
+        <p className="categories">Image:</p>
+        <input type="file" />
+      </div>
       <div className="genderCheckboxes">
         <p className="categories">Gender:</p>
         <label htmlFor="gender">
@@ -18,7 +20,7 @@ const AddForm = () => (
           Womens
         </label>
       </div>
-      <div className="checkboxes">
+      <div className="conditionCheckboxes">
         <p className="categories">Condition:</p>
         <label htmlFor="new-with-tags">
           <input
@@ -57,188 +59,199 @@ const AddForm = () => (
           Vintage
         </label>
       </div>
-      <p className="categories">Clothing Type:</p>
-      <label htmlFor="clothing-type">
-        <input
-          className={styles.input}
-          type="checkbox"
-          name="clothing-type"
-          value="crewneck"
-          id="crewneck"
-        />
-        Crewneck
-      </label>
-      <label htmlFor="clothing-type">
-        <input
-          className={styles.input}
-          type="checkbox"
-          name="clothing-type"
-          value="hoodie"
-          id="hoodie"
-        />
-        Hoodie
-      </label>
-      <label htmlFor="clothing-type">
-        <input
-          className={styles.input}
-          type="checkbox"
-          name="clothing-type"
-          value="jersey"
-          id="jersey"
-        />
-        Jersey
-      </label>
-      <label htmlFor="style">
-        <input
-          className={styles.input}
-          type="checkbox"
-          name="style"
-          value="jacket"
-          id="jacket"
-        />
-        Jacket
-      </label>
-      <label htmlFor="style">
-        <input
-          className={styles.input}
-          type="checkbox"
-          name="style"
-          value="flag"
-          id="flag"
-        />
-        Flag
-      </label>
-      <label htmlFor="style">
-        <input
-          className={styles.input}
-          type="checkbox"
-          name="style"
-          value="footwear"
-          id="footwear"
-        />
-        Footwear
-      </label>
-      <label htmlFor="style">
-        <input
-          className={styles.input}
-          type="checkbox"
-          name="style"
-          value="headgear"
-          id="headgear"
-        />
-        Headgear
-      </label>
-      <label htmlFor="style">
-        <input
-          className={styles.input}
-          type="checkbox"
-          name="style"
-          value="longsleeve"
-          id="longsleeve"
-        />
-        Longsleeve
-      </label>
-      <label htmlFor="style">
-        <input
-          className={styles.input}
-          type="checkbox"
-          name="style"
-          value="shortsleeve"
-          id="shortsleeve"
-        />
-        Shortsleeve
-      </label>
-      <label htmlFor="style">
-        <input
-          className={styles.input}
-          type="checkbox"
-          name="style"
-          value="shorts"
-          id="shorts"
-        />
-        Shorts
-      </label>
-      <label htmlFor="style">
-        <input
-          className={styles.input}
-          type="checkbox"
-          name="style"
-          value="sporting-equipment"
-          id="sporting-equipment"
-        />
-        Sporting equipment
-      </label>
-      <label htmlFor="style">
-        <input
-          className={styles.input}
-          type="checkbox"
-          name="style"
-          value="tshirt"
-          id="tshirt"
-        />
-        T-Shirt
-      </label>
-      <label htmlFor="style">
-        <input
-          className={styles.input}
-          type="checkbox"
-          name="style"
-          value="tank-top"
-          id="tank-top"
-        />
-        Tank top
-      </label>
-      <label htmlFor="style">
-        <input
-          className={styles.input}
-          type="checkbox"
-          name="style"
-          value="zipped"
-          id="zipped"
-        />
-        Zipped
-      </label>
-      <label htmlFor="style">
-        <input
-          className={styles.input}
-          type="checkbox"
-          name="style"
-          value="miscallaneous"
-          id="miscellaneous"
-        />
-        Miscellaneous
-      </label>
-      <p className="categories">Sizing:</p>
-      <label htmlFor="sizing">
-        <input type="checkbox" name="sizing" value="x-small" id="x-small" />
-        X-Small
-      </label>
-      <label htmlFor="sizing">
-        <input type="checkbox" name="sizing" value="small" id="small" />
-        Small
-      </label>
-      <label htmlFor="sizing">
-        <input type="checkbox" name="sizing" value="medium" id="medium" />
-        Medium
-      </label>
-      <label htmlFor="sizing">
-        <input type="checkbox" name="sizing" value="large" id="large" />
-        Large
-      </label>
-      <label htmlFor="sizing">
-        <input type="checkbox" name="sizing" value="x-large" id="x-large" />
-        X-Large
-      </label>
-      <label htmlFor="sizing">
-        <input type="checkbox" name="sizing" value="oversized" id="oversized" />
-        Oversized
-      </label>
-      <p className="categories">Price:</p>
-      <label htmlFor="pricing">
-        <input type="text" name="pricing" id="pricing" placeholder="$.00" />
-      </label>
+      <div className="clothingCheckboxes">
+        <p className="categories">Clothing Type:</p>
+        <label htmlFor="clothing-type">
+          <input
+            className={styles.input}
+            type="checkbox"
+            name="clothing-type"
+            value="crewneck"
+            id="crewneck"
+          />
+          Crewneck
+        </label>
+        <label htmlFor="clothing-type">
+          <input
+            className={styles.input}
+            type="checkbox"
+            name="clothing-type"
+            value="hoodie"
+            id="hoodie"
+          />
+          Hoodie
+        </label>
+        <label htmlFor="clothing-type">
+          <input
+            className={styles.input}
+            type="checkbox"
+            name="clothing-type"
+            value="jersey"
+            id="jersey"
+          />
+          Jersey
+        </label>
+        <label htmlFor="style">
+          <input
+            className={styles.input}
+            type="checkbox"
+            name="style"
+            value="jacket"
+            id="jacket"
+          />
+          Jacket
+        </label>
+        <label htmlFor="style">
+          <input
+            className={styles.input}
+            type="checkbox"
+            name="style"
+            value="flag"
+            id="flag"
+          />
+          Flag
+        </label>
+        <label htmlFor="style">
+          <input
+            className={styles.input}
+            type="checkbox"
+            name="style"
+            value="footwear"
+            id="footwear"
+          />
+          Footwear
+        </label>
+        <label htmlFor="style">
+          <input
+            className={styles.input}
+            type="checkbox"
+            name="style"
+            value="headgear"
+            id="headgear"
+          />
+          Headgear
+        </label>
+        <label htmlFor="style">
+          <input
+            className={styles.input}
+            type="checkbox"
+            name="style"
+            value="longsleeve"
+            id="longsleeve"
+          />
+          Longsleeve
+        </label>
+        <label htmlFor="style">
+          <input
+            className={styles.input}
+            type="checkbox"
+            name="style"
+            value="shortsleeve"
+            id="shortsleeve"
+          />
+          Shortsleeve
+        </label>
+        <label htmlFor="style">
+          <input
+            className={styles.input}
+            type="checkbox"
+            name="style"
+            value="shorts"
+            id="shorts"
+          />
+          Shorts
+        </label>
+        <label htmlFor="style">
+          <input
+            className={styles.input}
+            type="checkbox"
+            name="style"
+            value="sporting-equipment"
+            id="sporting-equipment"
+          />
+          Sporting equipment
+        </label>
+        <label htmlFor="style">
+          <input
+            className={styles.input}
+            type="checkbox"
+            name="style"
+            value="tshirt"
+            id="tshirt"
+          />
+          T-Shirt
+        </label>
+        <label htmlFor="style">
+          <input
+            className={styles.input}
+            type="checkbox"
+            name="style"
+            value="tank-top"
+            id="tank-top"
+          />
+          Tank top
+        </label>
+        <label htmlFor="style">
+          <input
+            className={styles.input}
+            type="checkbox"
+            name="style"
+            value="zipped"
+            id="zipped"
+          />
+          Zipped
+        </label>
+        <label htmlFor="style">
+          <input
+            className={styles.input}
+            type="checkbox"
+            name="style"
+            value="miscallaneous"
+            id="miscellaneous"
+          />
+          Miscellaneous
+        </label>
+      </div>
+      <div className="sizingCheckboxes">
+        <p className="categories">Sizing:</p>
+        <label htmlFor="sizing">
+          <input type="checkbox" name="sizing" value="x-small" id="x-small" />
+          X-Small
+        </label>
+        <label htmlFor="sizing">
+          <input type="checkbox" name="sizing" value="small" id="small" />
+          Small
+        </label>
+        <label htmlFor="sizing">
+          <input type="checkbox" name="sizing" value="medium" id="medium" />
+          Medium
+        </label>
+        <label htmlFor="sizing">
+          <input type="checkbox" name="sizing" value="large" id="large" />
+          Large
+        </label>
+        <label htmlFor="sizing">
+          <input type="checkbox" name="sizing" value="x-large" id="x-large" />
+          X-Large
+        </label>
+        <label htmlFor="sizing">
+          <input
+            type="checkbox"
+            name="sizing"
+            value="oversized"
+            id="oversized"
+          />
+          Oversized
+        </label>
+      </div>
+      <div className="pricingCheckboxes">
+        <p className="categories">Price:</p>
+        <label htmlFor="pricing">
+          <input type="text" name="pricing" id="pricing" placeholder="$.00" />
+        </label>
+      </div>
     </form>
-    <button type="submit">Submit</button>
+    <button className="button" type="submit">Submit</button>
   </div>
 );
 
