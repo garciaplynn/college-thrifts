9ecb995ae1ec3e594eaa85bc8b0827c3cdc50bea
charliedodds <charlesgdodds@hotmail.co.uk>
2021-05-05 14:55:05 +0100

    Make test super async

--- src/components/ProfileBottom/ProfileBottom.test.js
+++ src/components/ProfileBottom/ProfileBottom.test.js
@@ -6,12 +6,16 @@ import users from '../../resources/data/users';
 describe("profile bottom tests", () => {
   let component;
 
-  beforeEach(() => {
-    component = shallow(<ProfileBottom user={users[0]} />);
+  beforeEach(async () => {
+    component = await shallow(<ProfileBottom user={users[0]} />);
   });
 
-  it("should render selling by default", async () => {
-    const childrenLength = await component.find('.ClothingImage').children().length;
-    expect(childrenLength).toBe(0)
+  it("should render selling by default", async (done) => {
+    const asyncComponent = await component;
+    const asyncClothingImg = await asyncComponent.find('.ClothingImage');
+    const asyncChildren = await asyncClothingImg.children();
+    const asyncChildrenLength = await asyncChildren.length;
+    expect(asyncChildrenLength).toBe(0);
+    done();
   });
 });
