9b3ad104ecf4a5e9cc1523f53940bd525c2803a2
joe-fol <joefolkard@protonmail.com>
2021-05-06 14:33:19 +0100

    Added tests

--- src/components/Error/Error.test.js
+++ src/components/Error/Error.test.js
@@ -1,9 +1,21 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import Error from "./Error";
+import { shallow } from 'enzyme';
+
 
 describe("Error tests", () => {
+  let component;
+
+  beforeEach(() => {
+    component = shallow(<Error message='error'/>);
+  });
+
   it("should render", () => {
     expect(render(<Error />)).toBeTruthy();
   });
+
+  it("should display message prop", () => {
+    expect(component.find('p').text()).toBe('error');
+  })
 });
